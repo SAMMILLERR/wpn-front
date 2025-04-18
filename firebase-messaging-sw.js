fb1b5281bde55d4a3747acee168d9438c2cd0cd9
SAMMILLERR <saatvik.pandey.cd.che22@itbhu.ac.in>
2025-04-19 01:22:10 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,6 +2,20 @@
 importScripts('https://www.gstatic.com/firebasejs/9.6.7/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.6.7/firebase-messaging-compat.js');
 
+window.env = {
+  firebaseConfig: {
+    apiKey: "random-api-key",
+    authDomain: "...firebaseapp.com",
+    projectId: "your-project-id",
+    storageBucket: "your-storage-bucket.firebasestorage.app",
+    messagingSenderId: "0000000000",
+    appId: "a:b:0d0:a",
+    measurementId: "",
+    vapidKey: ""
+  },
+  backendUrl: "http://localhost:8000"
+};
+
 firebase.initializeApp(window.env.firebaseConfig);
 
 const messaging = firebase.messaging();
